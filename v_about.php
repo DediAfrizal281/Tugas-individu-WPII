20ef09d9d87ab60b4946ca23749f67426ee95bc6
DediAfrizal281 <91778533+DediAfrizal281@users.noreply.github.com>
2021-10-02 16:57:57 +0700

    Mengupload file tambahan pertemuan 3
    seluruh file ini di masukkan ke code igniter, application/views

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+ 	<h4>Nama</h4>
+ 	<ul type="disc">
+ 		<li>Nama Depan : Dedi</li>
+ 		<li>Nama Belakang : Afrizal</li>
+ 		<li>Nim : 19200660</li>
+ 	</ul>
+ <br>
+ 	<h4>Alamat</h4>
+ 	<ul type="none">
+ 		<li> Jln.Lapangan Tenis Meruya ilir no.8c</li>
+ 	</ul>
+ 
+ 	<h4>Tempat Lahir</h4>
+ 	<ul type="none">
+ 		<li>Jakarta</li>
+ 	</ul>
+ 	<h4>Olahraga Favorite</h4>
+ 	<ul type="square">
+ 		<li>Bulu tangkis</li>
+ 		<li>Renang</li>
+ 	</ul>
+</section>
---
+++ v_footer.php
@@ -0,0 +1,6 @@
+        <footer>
+			<a href="http://www.Rentalbuku.net">Sekian Terima Kasih</a>
+		</footer>
+	</div>
+</body>
+</html>
